b82e93223a10a7f7791bb3c74ec07226caf88736
Paul Tyng <ptyng@underarmour.com>
2017-11-14 10:13:13 -0500

    Add element unmarshaling

---
+++ unmarshal.go
@@ -0,0 +1,48 @@
+package xmlfrag
+
+import (
+	"bytes"
+	"encoding/xml"
+)
+
+func (el Element) start() xml.StartElement {
+	return xml.StartElement{
+		Name: el.XMLName,
+		Attr: el.Attr,
+	}
+}
+
+func (el Element) end() xml.EndElement {
+	return xml.EndElement{
+		Name: el.XMLName,
+	}
+}
+
+func (el Element) Unmarshal(v interface{}) error {
+	buf := bytes.NewBuffer(nil)
+	enc := xml.NewEncoder(buf)
+	// err := enc.EncodeToken(xml.ProcInst{Target: "xml"})
+	// if err != nil {
+	// 	return err
+	// }
+	err := enc.EncodeToken(el.start())
+	if err != nil {
+		return err
+	}
+	err = enc.Flush()
+	if err != nil {
+		return err
+	}
+	_, err = buf.WriteString(el.InnerXML)
+	if err != nil {
+		return err
+	}
+	_, err = buf.WriteString(el.Chardata)
+	if err != nil {
+		return err
+	}
+	enc.EncodeToken(el.end())
+	enc.Flush()
+	println(string(buf.Bytes()))
+	return xml.Unmarshal(buf.Bytes(), v)
+}
---
+++ unmarshal_test.go
@@ -0,0 +1,49 @@
+package xmlfrag
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestElementUnmarshal(t *testing.T) {
+	type simple struct {
+		Foo string `xml:"foo"`
+		Bar string `xml:"bar"`
+	}
+
+	type complex struct {
+		Foo string `xml:"foo,attr"`
+		Baz int    `xml:"bar>baz"`
+	}
+
+	{
+		expected := &simple{
+			Foo: "foo1",
+			Bar: "bar1",
+		}
+		actual := &simple{}
+		assert.NoError(t, Element{
+			XMLName:  xml.Name{Local: "Root"},
+			InnerXML: "<foo>foo1</foo><bar>bar1</bar>",
+		}.Unmarshal(actual))
+		assert.Equal(t, expected, actual)
+	}
+
+	{
+		expected := &complex{
+			Foo: "foo1",
+			Baz: 3,
+		}
+		actual := &complex{}
+		assert.NoError(t, Element{
+			XMLName: xml.Name{Local: "Root"},
+			Attr: []xml.Attr{
+				{Name: xml.Name{Local: "foo"}, Value: "foo1"},
+			},
+			InnerXML: "<bar><baz>3</baz></bar>",
+		}.Unmarshal(actual))
+		assert.Equal(t, expected, actual)
+	}
+}
